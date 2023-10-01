c639e23c7ff2a4923e0a992fcc09ce0f44efe048
Jessy Baki <bakijessy@gmail.com>
2023-10-01 10:59:05 +0000

    Add major changes to route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "@utils/database";
 
 const handler = NextAuth({
     providers: [
@@ -8,14 +9,19 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
-    async session() {
+    async session({ session }) {
         try {
+            await connectToDB();
 
+            //Check if a user exist
+            //if not create a new user
+            return true;
         } catch (err) {
-
+            console.log(err);
+            return false;
         }
     },
-    async profile() { }
+    async profile({ profile }) { }
 });
 
 export { handler as GET, handler as POST };
