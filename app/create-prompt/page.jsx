685a1dd9a0fd6426361b201214dff823f7a95157
Jessy Baki <bakijessy@gmail.com>
2023-10-01 12:02:19 +0000

    Render page to create prompt

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import Form from "@components/Form";
+
+const CreatePrompt = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
+
+  const createPrompt = async (e) => {};
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      submitting={submitting}
+      setPost={setPost}
+      handleSubmit={createPrompt}
+    />
+  );
+};
+
+export default CreatePrompt;
