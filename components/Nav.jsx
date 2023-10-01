8a3f8aec563c3c29449813ddbe82248dcdc00da5
Jessy Baki <bakijessy@gmail.com>
2023-10-01 01:45:25 +0000

    Update - Add major changes to entire project

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,10 +1,29 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import { signIn, signOut, getProviders, useSession } from "next-auth/react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const isUser
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Promptopia</p>
+      </Link>
+
+      {/* MObile Navigation */}
+      <div className="sm:flex hidden"></div>
+    </nav>
+  );
 };
 
 export default Nav;
