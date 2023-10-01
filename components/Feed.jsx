4c30b64c57078185546c75d562e12e434c30f10c
Jessy Baki <bakijessy@gmail.com>
2023-10-01 13:19:39 +0000

    Use implicit function definition instead

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -43,13 +43,13 @@ const Feed = () => {
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
     <div className="mt-16 prompt_layout">
-      {data.map((post) => {
+      {data.map((post) => (
         <PromptCard
           key={post._id}
           post={post}
           handleTagClick={handleTagClick}
-        />;
-      })}
+        />
+      ))}
     </div>
   );
 };
