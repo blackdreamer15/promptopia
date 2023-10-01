6c476d8d5d0cddf25f561fd1579fc1e8e9e339dc
Jessy Baki <bakijessy@gmail.com>
2023-10-01 11:07:02 +0000

    Add more changes to route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@utils/database";
+import User from "@models/user";
+import { profile } from "console";
 
 const handler = NextAuth({
     providers: [
@@ -10,18 +12,30 @@ const handler = NextAuth({
         })
     ],
     async session({ session }) {
+        const userSession = await User.findOne({ email: session.user.email });
+        session.user.id = userSession._id.toString();
+        return session;
+    },
+    async profile({ profile }) {
         try {
             await connectToDB();
 
             //Check if a user exist
+            const userExists = await User.findOne({ email: profile.email });
             //if not create a new user
+            if (!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture
+                })
+            }
             return true;
         } catch (err) {
             console.log(err);
             return false;
         }
-    },
-    async profile({ profile }) { }
+    }
 });
 
 export { handler as GET, handler as POST };
