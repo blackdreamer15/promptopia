2d6f2a40e75ea36b1e8cc705c942159149e8db1c
Jessy Baki <bakijessy@gmail.com>
2023-10-01 12:01:37 +0000

    Add changes to Form.jsx

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,5 +1,11 @@
-const Form = () => {
-  return <div>Form</div>;
+import Link from "next/link";
+
+const Form = ({ type, post, submitting, setPost, handleSubmit }) => {
+  return (
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1>{type} Post</h1>
+    </section>
+  );
 };
 
 export default Form;
