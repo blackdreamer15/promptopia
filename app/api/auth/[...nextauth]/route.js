58867be34ad7dd4dd53e1795f93decc6434539b2
Jessy Baki <bakijessy@gmail.com>
2023-10-01 11:44:28 +0000

    Fix - Add callback handler

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,29 +11,31 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
-    async session({ session }) {
-        const userSession = await User.findOne({ email: session.user.email });
-        session.user.id = userSession._id.toString();
-        return session;
-    },
-    async profile({ profile }) {
-        try {
-            await connectToDB();
+    callbacks: {
+        async session({ session }) {
+            const userSession = await User.findOne({ email: session.user.email });
+            session.user.id = userSession._id.toString();
+            return session;
+        },
+        async profile({ profile }) {
+            try {
+                await connectToDB();
 
-            //Check if a user exist
-            const userExists = await User.findOne({ email: profile.email });
-            //if not create a new user
-            if (!userExists) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture
-                })
+                //Check if a user exist
+                const userExists = await User.findOne({ email: profile.email });
+                //if not create a new user
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+                return true;
+            } catch (err) {
+                console.log(err);
+                return false;
             }
-            return true;
-        } catch (err) {
-            console.log(err);
-            return false;
         }
     }
 });
