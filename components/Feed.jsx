0b2581f1b0b6b272ea19eec920e20d3d7de6a42a
Jessy Baki <bakijessy@gmail.com>
2023-10-01 13:10:19 +0000

    Add a form to Feed component to  make search

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,5 +1,28 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchChange = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input"
+        />
+      </form>
+    </section>
+  );
 };
 
 export default Feed;
