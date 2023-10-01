2cdae7595c5096b1119ada8e89e02df02cf3cd0f
Jessy Baki <bakijessy@gmail.com>
2023-10-01 14:57:26 +0000

    Upd: Add MyProfile component in page.jsx

---
+++ app/api/profile/page.jsx
@@ -0,0 +1,38 @@
+"useClient";
+
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+
+  const handleEdit = () => {};
+
+  const handleDelete = async () => {};
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const res = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await res.json();
+
+      setPost(data);
+    };
+
+    if (session?.user.id) fetchPost();
+  }, []);
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default MyProfile;
