173f1309689f5c09fdfd40b069fe7b084f84289a
Jessy Baki <bakijessy@gmail.com>
2023-10-01 15:09:29 +0000

    Add endpoint to update prompt

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -15,3 +15,24 @@ export const GET = async (req, { params }) => {
 }
 
 
+export const PATCH = async (req, { params }) => {
+    const { prompt, tag } = await req.json();
+
+    try {
+        await connectToDB();
+        const existingPrompt = await Prompt.findById(params.id);
+
+        if (!existingPrompt) return new Response("Prompt not found", { status: 404 })
+
+        existingPrompt.prompt = prompt;
+        existingPrompt.tag = tag;
+
+        await existingPrompt.save();
+
+        return new Response(JSON.stringify(prompt), { status: 200 })
+    } catch (error) {
+        return new Response("Failed to update the prompt", { status: 500 })
+    }
+}
+
+
